068f289d472ab16713c0f23898f7cfd5684e614c
MauricioGeinfo <mauricio@geinfo.com.br>
2019-12-04 11:33:34 -0300

    first commit

---
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -0,0 +1,18 @@
+CREATE TABLE pessoa (
+	codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	ativo BOOLEAN NOT NULL,
+	logradouro VARCHAR(50),
+	numero INT,
+	complemento VARCHAR(50),
+	bairro VARCHAR(50),
+	cep VARCHAR(11),
+	cidade VARCHAR(50),
+	estado CHAR(2)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep) VALUES ('Maurício', 'true', 'Interior', 0, 'Casa', 'Linha Caixa d\'agua', '89514-899');
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep) VALUES ('Ketlin', 'true', 'Martello', 184, 'Casa', 'João Angeli', '89500-899');
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep) VALUES ('Paula', 'false', NULL, 0, NULL, NULL, NULL);
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep) VALUES ('Gabriel', 'true', 'Belo', 226, 'Casa', 'Quebrada', '89514-899');
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep) VALUES ('Delmison', 'true', NULL, 0, NULL, NULL, NULL);
